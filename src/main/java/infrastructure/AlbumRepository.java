9f6deb601d0e9c2a3a0d23d77e7bc5222cdb3728
achimunterkofler <achimunterkofler@gmx.at>
2022-03-19 22:28:49 +0100

    return string

--- src/main/java/infrastructure/AlbumRepository.java
+++ src/main/java/infrastructure/AlbumRepository.java
@@ -1,12 +1,13 @@
 package infrastructure;
 
 import domain.Album;
+import domain.Product;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public interface AlbumRepository {
 
-    List<Album> findAlbum(String musicName) throws SQLException, ClassNotFoundException;
+    List<String> findAlbum(String musicName) throws SQLException, ClassNotFoundException;
 
 }
--- src/main/java/infrastructure/AlbumRepositoryImpl.java
+++ src/main/java/infrastructure/AlbumRepositoryImpl.java
@@ -1,16 +1,21 @@
 package infrastructure;
 
 import domain.Album;
+import domain.Artist;
+import domain.Product;
+import domain.Song;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.sql.*;
 
 public class AlbumRepositoryImpl implements AlbumRepository {
 
     @Override
-    public List<Album> findAlbum(String musicName) throws SQLException, ClassNotFoundException {
-
+    public List<String> findAlbum(String musicName) throws SQLException, ClassNotFoundException {
+        //List<Product> result = new ArrayList<>();
+        List<String> result = new ArrayList<>();
 
         try {
             Class.forName("org.postgresql.Driver");
@@ -22,25 +27,26 @@ public class AlbumRepositoryImpl implements AlbumRepository {
             Connection con = DriverManager.getConnection(url, user, password);
             Statement st = con.createStatement();
 
-            String fetchquery = "select * from product";
+            String fetchquery = "select * from tbl_product";
 
             ResultSet rs = st.executeQuery(fetchquery);
 
-
             while (rs.next()) {
-                System.out.println(
-                        rs.getString("title")
-                );
+                if (rs.getString("title").toLowerCase().equals(musicName.toLowerCase())){
+                    //result = new Song.SongBuilder(rs.getString("genre"), new List<Artist>());
+                    result.add(rs.getString("title"));
+                }
             }
+
             st.close();
             con.close();
-        } catch(ClassNotFoundException e){
-            e.printStackTrace();
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-        }
-        return null;
-    }
 
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    return result;
+    }
 }
