9353f9d7355b576234ad6952dba2b06538765177
Benjamin Mayer <lolgame99@gmail.com>
2022-03-22 10:31:00 +0100

    changed domain objects and mapping

--- src/main/java/domain/Employee.java
+++ src/main/java/domain/Employee.java
@@ -12,19 +12,10 @@ public class Employee {
     private EmployeeId employeeId;
     private String name;
     private String username;
-    private ShoppingCart shoppingCart;
 
     public Employee(EmployeeId employeeId, String name, String username) {
         this.employeeId = employeeId;
         this.name = name;
         this.username = username;
-        this.shoppingCart = new ShoppingCart();
-    }
-
-    public Employee(EmployeeId employeeId, String name, String username, ShoppingCart shoppingCart) {
-        this.employeeId = employeeId;
-        this.name = name;
-        this.username = username;
-        this.shoppingCart = shoppingCart;
     }
 }
--- src/main/java/domain/ShoppingCart.java
+++ src/main/java/domain/ShoppingCart.java
@@ -1,15 +1,29 @@
 package domain;
 
+import lombok.Getter;
+
 import java.util.LinkedList;
 import java.util.List;
 
+@Getter
 public class ShoppingCart {
 
     private List<LineItem> lineItems;
+    private Employee employee;
 
     public ShoppingCart(){this.lineItems = new LinkedList<>();};
 
-    public ShoppingCart(List<LineItem> lineItems) {
+    public ShoppingCart(Employee employee){
+        this.employee = employee;
+        this.lineItems = new LinkedList<>();
+    }
+
+    public ShoppingCart(Employee employee, List<LineItem> lineItems) {
         this.lineItems = lineItems;
+        this.employee = employee;
+    }
+
+    public void addLineItem(LineItem item){
+        this.lineItems.add(item);
     }
 }
