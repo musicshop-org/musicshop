8f5cdb3e614f7481b7a07df54b1f8b9efc0dcd78
mme8372 <matthias.meier@students.fhv.at>
2022-03-24 13:55:23 +0100

    minor change

--- src/main/java/application/ProductServiceImpl.java
+++ src/main/java/application/ProductServiceImpl.java
@@ -19,6 +19,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
+
 public class ProductServiceImpl extends UnicastRemoteObject implements ProductService {
 
     ProductRepository productRepository = new ProductRepositoryImpl();
