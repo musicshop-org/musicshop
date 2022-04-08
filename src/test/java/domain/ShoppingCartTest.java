0cc15035dafee8ab09155dd2762ab73656375cd7
Marco Prescher <m.prescher@gantner-instruments.com>
2022-04-08 10:04:59 +0200

    added clear cart functionality + tests

--- src/test/java/domain/ShoppingCartTest.java
+++ src/test/java/domain/ShoppingCartTest.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShoppingCartTest {
+
     @Test
     void when_lineItem_added_return_equal() {
         //given
@@ -21,4 +22,21 @@ public class ShoppingCartTest {
         //then
         assertEquals(expectedItem, cart.getCartLineItems().get(0));
     }
+
+    @Test
+    void when_clearCart_return_empty() {
+        //given
+        ShoppingCart cart = new ShoppingCart(UUID.randomUUID().toString());
+        CartLineItem expectedItem = new CartLineItem(MediumType.CD, "24K Magic", 12, BigDecimal.valueOf(18));
+        cart.addLineItem(expectedItem);
+
+        int expected = 0;
+
+        //when
+        cart.clear();
+
+        //then
+        assertEquals(expected, cart.getCartLineItems().size());
+    }
+
 }
