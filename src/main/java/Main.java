2218156a10f5319e3a2eea91f608b3ab8e90bad4
fabian_egartner <fabian.egartner@hotmail.com>
2022-04-11 22:58:46 +0200

    implemented findAlbumByAlbumTitleAndMedium (for service and repo)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,6 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-
 public class Main {
     public static void main(String[] args) {
 
