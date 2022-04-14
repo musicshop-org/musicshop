1ebe5a9d94a537c6c7b241e2ebfd15ea2b93a404
Fabian Egartner <fabian.egartner@hotmail.com>
2022-04-14 13:41:16 +0200

    modified shared lib

---
+++ src/main/java/application/MessageProducerServiceImpl.java
@@ -0,0 +1,14 @@
+package application;
+
+import sharedrmi.application.api.MessageProducerService;
+
+import java.rmi.RemoteException;
+import java.util.List;
+
+public class MessageProducerServiceImpl implements MessageProducerService {
+
+    @Override
+    public void publish(List<String> list, String s, String s1) throws RemoteException {
+
+    }
+}
