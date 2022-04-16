4e6698354408df2d0de6c3a9233ff773963c83c0
Fabian Egartner <fabian.egartner@hotmail.com>
2022-04-17 00:32:16 +0200

    hibernate: User, Topic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,42 @@
 
 import communication.rmi.RMIControllerFactoryImpl;
+import domain.Topic;
+import domain.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 import sharedrmi.communication.rmi.RMIControllerFactory;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        try {
-            RMIControllerFactory rmiControllerFactory = RMIControllerFactoryImpl.getInstance();
-            LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
-            Naming.rebind("rmi://localhost/RMIControllerFactory", rmiControllerFactory);
+//        try {
+//            RMIControllerFactory rmiControllerFactory = RMIControllerFactoryImpl.getInstance();
+//            LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
+//            Naming.rebind("rmi://localhost/RMIControllerFactory", rmiControllerFactory);
+//
+//            System.out.println("Listening on port " + Registry.REGISTRY_PORT);
+//
+//        } catch (RemoteException | MalformedURLException e) {
+//            e.printStackTrace();
+//        }
 
-            System.out.println("Listening on port " + Registry.REGISTRY_PORT);
+        User user = new User("admin", List.of(new Topic("system"), new Topic("order")));
 
-        } catch (RemoteException | MalformedURLException e) {
-            e.printStackTrace();
-        }
+        final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.persist(user);
+        transaction.commit();
+        session.close();
+
     }
 }
