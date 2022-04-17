a12bede693ce778704dc7645e835ec778d187dc1
fabian_egartner <fabian.egartner@hotmail.com>
2022-04-17 11:43:46 +0200

    UserRepositoryImpl

--- src/main/java/infrastructure/UserRepositoryImpl.java
+++ src/main/java/infrastructure/UserRepositoryImpl.java
@@ -16,7 +16,7 @@ public class UserRepositoryImpl implements UserRepository {
     public Optional<User> findUserByUsername(String username) {
         Session session = sessionFactory.openSession();
 
-        User user = session.createQuery()
+        User user = session.createQuery("from User")
                 .setParameter("username", username)
                 .getSingleResultOrNull();
 
