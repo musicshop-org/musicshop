b53672134286bb33ee9572770291819f74722e55
Fabian Egartner <fabian.egartner@hotmail.com>
2022-04-17 11:34:37 +0200

    hibernate: User, Topic

--- src/main/java/infrastructure/UserRepositoryImpl.java
+++ src/main/java/infrastructure/UserRepositoryImpl.java
@@ -2,13 +2,19 @@ package infrastructure;
 
 import domain.User;
 import domain.repositories.UserRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 import java.util.Optional;
 
 public class UserRepositoryImpl implements UserRepository {
 
+    private final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
     @Override
     public Optional<User> findUserByUsername(String username) {
+        Session session = sessionFactory.openSession();
         return Optional.empty();
     }
     //teet
