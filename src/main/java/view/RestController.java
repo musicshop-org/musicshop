1ce58ea50b5713432debc6a3c472996d3e985a4e
Fabian Egartner <fabian.egartner@hotmail.com>
2022-04-29 14:05:47 +0200

    REST works

--- src/main/java/view/RestController.java
+++ src/main/java/view/RestController.java
@@ -2,15 +2,16 @@ package view;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 @Path("")
 public class RestController {
 
     @GET
-    @Path("/hello-world")
+    @Path("/getAlbums/{albumTitle}")
     @Produces("text/plain")
-    public String hello() {
-        return "hello world :)";
+    public String hello(@PathParam("albumTitle") String albumTitle) {
+        return "Album: " + albumTitle;
     }
 }
