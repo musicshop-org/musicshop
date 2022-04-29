d1fe4717bc8f748488d0c2bca4ff7c06e0bb5c11
Fabian Egartner <fabian.egartner@hotmail.com>
2022-04-29 13:59:15 +0200

    REST works

--- src/main/java/view/RestController.java
+++ src/main/java/view/RestController.java
@@ -1,7 +1,16 @@
 package view;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
-@Path("/rest/music")
+@Path("")
 public class RestController {
+
+    @GET
+    @Path("/hello-world")
+    @Produces("text/plain")
+    public String hello() {
+        return "hello world :)";
+    }
 }
---
+++ src/main/java/view/ServerApplication.java
@@ -0,0 +1,8 @@
+package view;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class ServerApplication extends Application {
+}
