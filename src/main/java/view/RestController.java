d3f333f9cd9a3f4434961d2b4f45b7e7267082ed
schlaumeierisch <matthias.meier97@hotmail.com>
2022-05-03 11:29:41 +0200

    display & clear shopping cart REST

--- src/main/java/view/RestController.java
+++ src/main/java/view/RestController.java
@@ -5,6 +5,7 @@ import application.ShoppingCartServiceImpl;
 import sharedrmi.application.api.ProductService;
 import sharedrmi.application.api.ShoppingCartService;
 import sharedrmi.application.dto.AlbumDTO;
+import sharedrmi.application.dto.ShoppingCartDTO;
 
 import javax.naming.NoPermissionException;
 import javax.ws.rs.*;
@@ -15,14 +16,14 @@ import java.util.List;
 public class RestController {
 
     private final ProductService productService = new ProductServiceImpl();
-    private final ShoppingCartService shoppingCartService = new ShoppingCartServiceImpl();
+    private final ShoppingCartService shoppingCartService = new ShoppingCartServiceImpl("PythonTestClient");
 
     public RestController() throws RemoteException {}
 
     @GET
     @Produces("text/html")
     public String welcome() {
-        return "<h1> welcome to our musicshop :) </h1>";
+        return "<h1> welcome to our music shop :) </h1>";
     }
 
     @GET
@@ -40,6 +41,35 @@ public class RestController {
         // needed: title of album, medium type, price, stock, quantity
         try {
             shoppingCartService.addProductToCart(album, album.getQuantityToAddToCart());
+        } catch (RemoteException | NoPermissionException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    @GET
+    @Path("/shoppingCart/display")
+    @Produces("application/json")
+    public ShoppingCartDTO displayShoppingCart() {
+
+        try {
+            return shoppingCartService.getCart();
+        } catch (RemoteException | NoPermissionException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    @GET
+    @Path("/shoppingCart/clear")
+    @Produces("application/json")
+    public boolean clearShoppingCart() {
+
+        try {
+            shoppingCartService.clearCart();
         } catch (RemoteException | NoPermissionException e) {
             e.printStackTrace();
             return false;
