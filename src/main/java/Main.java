dc62be9bf0b2c49404f61eca34c230020660aa51
Fabian Egartner <fabian.egartner@hotmail.com>
2022-05-05 20:05:19 +0200

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,24 +10,6 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
     public static void main(String[] args) {
-
-//        // customer
-//        String emailAddress = "AdelinaLandmann1987@email.test";
-//        String pw = "password04";
-//
-//        // licensee
-//        String emailAddress2 = "CharlieBittner2006@email.test";
-//        String pw2 = "password03";
-//
-//        // employee
-//        String emailAddress3 = "essiga";
-//        String pw3 = "password01";
-//
-//        RestLoginService restLoginService = new RestLoginServiceImpl();
-//        restLoginService.checkCredentials(emailAddress, pw);
-//        restLoginService.getRole(emailAddress3);
-
-
 //        try {
 //            System.setProperty("java.rmi.server.hostname", "localhost");
 //            //System.setSecurityManager(new SecurityManager());
@@ -42,38 +24,5 @@ public class Main {
 //        } catch (RemoteException | MalformedURLException e) {
 //            e.printStackTrace();
 //        }
-
-            String jwt = JwtManager.createJWT("Test", 900000);
-            boolean notOkay = JwtManager.isValidToken("abc");
-            boolean notOkay2 = JwtManager.isValidToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
-            boolean okay = JwtManager.isValidToken(jwt);
-
-            System.out.println(notOkay);
-            System.out.println(notOkay2);
-            System.out.println(okay);
-
-//        // create JWT
-//        String emailAddress = "AdelinaLandmann1987@email.test";
-//        String pw = "password04";
-//
-//        String emailAddress2 = "CharlieBittner2006@email.test";
-//        String pw2 = "password03";
-//
-//        String username = "Test_Customer";
-//
-//        long expiration = 900000;
-//        String jwtToken = JwtManager.createJWT(emailAddress2, expiration);
-//
-//        System.out.println("Generated_Token:" + jwtToken);
-//
-//        System.out.println(JwtManager.getId(jwtToken));
-//        System.out.println(JwtManager.getIssuedAt(jwtToken));
-//        System.out.println(JwtManager.getExpiration(jwtToken));
-//        System.out.println(JwtManager.getIssuer(jwtToken));
-//        System.out.println(JwtManager.getEmailAddress(jwtToken));
-//        System.out.println(JwtManager.getRoles(jwtToken));
-//
-//        // decode JWT
-//        System.out.println(JwtManager.decodeJWT(jwtToken));
     }
 }
