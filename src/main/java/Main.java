038449fb6d64615e56e64030da0193ae17554657
Fabian Egartner <fabian.egartner@hotmail.com>
2022-05-10 16:30:45 +0200

    Shared Lib

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,7 @@
-
-import application.InvoiceServiceImpl;
-import sharedrmi.application.api.InvoiceService;
-import sharedrmi.application.dto.InvoiceDTO;
-import sharedrmi.application.dto.InvoiceLineItemDTO;
 import sharedrmi.application.exceptions.AlbumNotFoundException;
 import sharedrmi.application.exceptions.NotEnoughStockException;
-import sharedrmi.domain.enums.MediumType;
-import sharedrmi.domain.enums.PaymentMethod;
-import sharedrmi.domain.valueobjects.Address;
-import sharedrmi.domain.valueobjects.CustomerData;
-import sharedrmi.domain.valueobjects.InvoiceId;
 
 import javax.naming.NoPermissionException;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
 
 public class Main {
 
@@ -34,26 +21,26 @@ public class Main {
 //            e.printStackTrace();
 //        }
 
-        InvoiceService invoiceService = new InvoiceServiceImpl();
-        invoiceService.createInvoice(
-                InvoiceDTO.builder()
-                        .invoiceId(new InvoiceId(666777))
-                        .invoiceLineItems(List.of(new InvoiceLineItemDTO(MediumType.CD, "Thriller", 1, BigDecimal.TEN, 0)))
-                        .paymentMethod(PaymentMethod.CASH)
-                        .date(LocalDate.now())
-                        .customerData(
-                                CustomerData.builder()
-                                        .firstName("Fabian")
-                                        .lastName("Egartner")
-                                        .email("fabian.egartner@test.at")
-                                        .address(Address.builder()
-                                                .addressCountry("AT")
-                                                .addressLocality("Lustenau")
-                                                .postalCode("6890")
-                                                .streetAddress("Teststrasse")
-                                                .build())
-                                        .build())
-                        .build()
-                );
+//        InvoiceService invoiceService = new InvoiceServiceImpl();
+//        invoiceService.createInvoice(
+//                InvoiceDTO.builder()
+//                        .invoiceId(new InvoiceId(666777))
+//                        .invoiceLineItems(List.of(new InvoiceLineItemDTO(MediumType.CD, "Thriller", 1, BigDecimal.TEN, 0)))
+//                        .paymentMethod(PaymentMethod.CASH)
+//                        .date(LocalDate.now())
+//                        .customerData(
+//                                CustomerData.builder()
+//                                        .firstName("Fabian")
+//                                        .lastName("Egartner")
+//                                        .email("fabian.egartner@test.at")
+//                                        .address(Address.builder()
+//                                                .addressCountry("AT")
+//                                                .addressLocality("Lustenau")
+//                                                .postalCode("6890")
+//                                                .streetAddress("Teststrasse")
+//                                                .build())
+//                                        .build())
+//                        .build()
+//                );
     }
 }
