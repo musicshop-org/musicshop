016c6d7ce0bb67663d51d81fb242925841e4219a
prescherm <m.prescher@gantner-instruments.com>
2022-06-02 12:44:06 +0200

    removed unused code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,23 +16,6 @@ import java.util.Set;
 
 public class Main {
 
-    public static void main(String[] args) throws AlbumNotFoundException, NoPermissionException, NotEnoughStockException {
-//        try {
-//            System.setProperty("java.rmi.server.hostname", "localhost");
-//            //System.setSecurityManager(new SecurityManager());
-//
-//
-//            RMIControllerFactory rmiControllerFactory = RMIControllerFactoryImpl.getInstance();
-//            LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
-//            Naming.rebind("rmi://localhost/RMIControllerFactory", rmiControllerFactory);
-//
-//            System.out.println("Listening on port " + Registry.REGISTRY_PORT);
-//
-//        } catch (RemoteException | MalformedURLException e) {
-//            e.printStackTrace();
-//        }
-
-//        ProductRepository productRepository = new ProductRepositoryImpl();
-//        List<Song> songs = productRepository.findSongsByTitle("beautiful");
+    public static void main(String[] args) {
     }
 }
